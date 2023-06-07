6ecfbe88ee06e00fcd7d0195be1943fcc2b7cb02
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-07 16:35:44 -0700

    task4

--- python_module/3-task.py
+++ python_module/3-task.py
@@ -6,6 +6,6 @@ argument = sys.argv[1:]
 num_argument = len(argument)
 
 for i in range(num_argument):
-    sum += int(argument[i])
+    sum = sum + int(argument[i])
 if __name__ == '__main__':
     print(sum)
