6858be11d38b045ff2c33462190a32df70a59c81
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-07 16:11:52 -0700

    task3

---
+++ python_module/3-task.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+sum = 0
+argument = sys.argv[1:]
+num_argument = len(argument)
+
+for i in range(num_argument):
+    sum += int(argument[i])
+if __name__ == '__main__':
+    print(sum)
